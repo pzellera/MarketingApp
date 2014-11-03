5068caaf47777dd55ada7f7b3cdc0c8f33f085b3
Peyton Mikkel Zeller-Av <peytav13@yahoo.com>
2014-11-03 15:58:15 -0600

    Created functionality for Register Button (haven't tested yet)

--- app/src/main/java/com/coffehauselabs/nodelogin/LoginActivity.java
+++ app/src/main/java/com/coffehauselabs/nodelogin/LoginActivity.java
@@ -1,19 +1,33 @@
 package com.coffehauselabs.nodelogin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity{
+    Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+        setContentView(R.layout.activity_login);    //Get content view from activity_login
 
+        regButton = (Button) findViewById(R.id.register);   //locate button in activity_login
+
+       //Capture regButton clicks
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Start activity_register
+                Intent myIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/coffehauselabs/nodelogin/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.coffehauselabs.nodelogin;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from activity_register
+        setContentView(R.layout.activity_register);
+    }
+}
