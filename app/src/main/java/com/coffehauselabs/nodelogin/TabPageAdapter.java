98113b309e4196ef4fdc712ba48663e164dc6f51
Peyton Mikkel Zeller-Av <peytav13@yahoo.com>
2014-11-15 17:35:46 -0600

    made frags for welcome screen, fix support library

---
+++ app/src/main/java/com/coffehauselabs/nodelogin/TabPageAdapter.java
@@ -0,0 +1,8 @@
+package com.coffehauselabs.nodelogin;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPageAdapter {
+}
